3a31d4b0a55a9826b778cb6e9ba18ac692a7c054
rosapalli <47088743+rosapalli@users.noreply.github.com>
2019-03-30 16:08:22 +0000

    Update item.class.php

--- Exercise21/resources/library/classes/Item/item.class.php
+++ Exercise21/resources/library/classes/Item/item.class.php
@@ -8,24 +8,15 @@ class abstract Item {
     private $location;
     private $status;
     
-    public function __construct($title, $language, $genre, $location, $loanLength, $location, $status) {
-        $this->title = $title;    
-        $this->language = $language; 
-        $this->genre = $genre; 
-        $this->loanLength = 1;
-        $this->location = $location;
-        $this->status = $status;
-    }
+    public abstract function setStatus($status);
     
-    public abstract function set_status ($newStatus) {
-    }
-   
-    public function get_status() {
-        return $this->status;
-    }
+    public abstract function getDetails();
     
-    public function get_location(){
-        return $this->location;
-    }
-} 
-
+    public abstract function borrowItem($member);
+    
+    public abstract function returnItem($member);
+    
+    public function getLocation() {
+        echo  $itemLocation = "The location for '$this->title' is: $this->location.";
+    }    
+}
