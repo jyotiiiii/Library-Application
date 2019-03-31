d080165228c0ef95c846b56ff81ee3fb6ce5ca43
ErikoRebeccaKobayashi <48835950+ErikoRebeccaKobayashi@users.noreply.github.com>
2019-03-31 20:28:29 +0100

    Log In and Log In Error
    HTML Log In and Log Error Pages, not including CSS

---
+++ LogInError.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .button {
+        background-color: white;
+        border: 2px solid green;
+        color: black;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        border-radius: 8px;
+    </style>
+    <title>Login Error</title>
+  </head>
+  <body>
+        <div class="error" align="center" style="margin-top: 80px;">
+            <div class="container" >
+                <div class="row">
+                <div class="container-fluid" class="text-center" class="align-center">
+                    <h1>Uh oh!</h1><br>
+                </div>
+                    </div>
+                    <div class="container-fluid" class="text-center" class="align-center"> <br>
+                    <p>Your log in is not working, please try again or contact the library</p>
+                    <iframe src="https://giphy.com/embed/vqgVGAol6Upj2" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                    <br><button type="submit" class="button">Try again</button>
+                    <button type="submit" class="button">Contact us</button>
+                </div>
+            </div>
+        </div>      
+</div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
---
+++ LogInPage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Log In Page</title>
+         <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <div style="margin-top: 80px;">
+  <h2 class="text-center">Log In To Whakatane Public Library</h2>
+  <div id="login-row" class="row justify-content-center align-items-center">
+      <div id="login-column" class="col-md-6 col-md-offset-3">
+                    <div id="login-box" class="col-md-6 col-md-offset-3">
+  <form action="" method="post">
+    <div class="form-group">
+      <label for="email">Username:</label>
+      <input type="text" class="form-control" id="username" placeholder="Username" name="Username" autofocus="true">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="remember"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+    <button type="submit" class="btn btn-default">Register</button>
+  </form>
+  </body>
+</html>
