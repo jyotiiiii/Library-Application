4ef68c1734e809aa53bdcf928083d3eabc18edd3
rosapalli <47088743+rosapalli@users.noreply.github.com>
2019-04-06 09:52:36 +0100

    Delete Interfaces.php

--- LibraryApp/Interfaces.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface Search {
-    public function searchByTitle(string $search); 
-    public function searchByLanguage(string $search);    
-    public function searchByGenre(string $search);
-    public function searchByStatus(string $search);
-    public function searchAvailableByTitle(string $earch);
-}
